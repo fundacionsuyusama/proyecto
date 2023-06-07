d78a425b2572bc9d5f3829524815f5809d0e819f
Juan David Rojas <juanrojas043@gmail.com>
2023-06-07 10:24:09 -0500

    Se quito la barra del url

--- tareas/urls.py
+++ tareas/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
   path('', views.user_login, name='login'),
   path('logout/', LogoutView.as_view(template_name='login/logout.html'), name='logout'),
   path('index/', views.index, name='index'),
-  path('home/', views.home, name='home'),
-  path('crear_resultado/', views.crear_resultado, name='crear_resultado'),
+  path('home', views.home, name='home'),
+  path('crear_resultado', views.crear_resultado, name='crear_resultado'),
 ]
