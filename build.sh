b028dd59857a5770708c3e466e27327bac99c9a8
Juan David Rojas <juanrojas043@gmail.com>
2023-06-06 22:06:24 -0500

    delete code

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('david', 'david@example.com', 'david')" | python manage.py shell
-
