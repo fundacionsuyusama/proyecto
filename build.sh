1541dad38e38cf9e12dd3efb1a46237621c30712
Juan David Rojas <juanrojas043@gmail.com>
2023-08-01 09:53:26 -0500

    Nuevo BD

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('david', 'david@example.com', 'david')" | python manage.py shell
