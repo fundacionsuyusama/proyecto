5aea485b29aeaacfe03440c0dd367be43ae45fdb
Juan David Rojas <juanrojas043@gmail.com>
2023-10-12 08:31:40 -0500

    New Base

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('david', 'david@example.com', 'david')" | python manage.py shell
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
--- tareas/admin.py
+++ tareas/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Resultado)
-admin.site.register(Actividad)
-admin.site.register(Seccion)
-admin.site.register(Avance)
-admin.site.register(Dificultad)
-admin.site.register(Alternativa)
-admin.site.register(Cumplido)
-admin.site.register(Proceso)
-admin.site.register(Urgente)
-admin.site.register(Proyecto)
+# admin.site.register(Resultado)
+# admin.site.register(Actividad)
+# admin.site.register(Seccion)
+# admin.site.register(Avance)
+# admin.site.register(Dificultad)
+# admin.site.register(Alternativa)
+# admin.site.register(Cumplido)
+# admin.site.register(Proceso)
+# admin.site.register(Urgente)
+# admin.site.register(Proyecto)
